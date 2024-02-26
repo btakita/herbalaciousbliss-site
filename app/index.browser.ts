c4d8b577230619301ef3ad7c129d698ac72f4a66
Brian Takita <brian.takita@gmail.com>
2024-02-25 22:03:22 -0500

    /: browser: ∋ single_hyop: ...site_header_hyops

--- app/index.browser.ts
+++ app/index.browser.ts
@@ -1,7 +1,7 @@
-import { site__header__hyop } from '@btakita/ui--browser--herbaliciousbliss/layout'
+import * as site_header_hyops from '@btakita/ui--browser--herbaliciousbliss/layout'
 import { single_hyop } from 'relementjs/browser/hy'
 window.addEventListener('load', ()=>{
 	single_hyop(document, {
-		site__header__hyop,
+		...site_header_hyops,
 	})
 })
