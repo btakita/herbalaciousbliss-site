62c42a9069938c2ae3ab2fa3f4878597c3136cb9
Brian Takita <brian.takita@gmail.com>
2024-02-29 00:13:31 -0500

    ./db: migrate: fix: ∋ sqlite_db__set: - ∋ sqlite_db__name__set

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,11 +1,14 @@
 import { drizzle_db_ } from '@rappstack/domain--server/drizzle'
-import { sqlite_db__name__set } from '@rappstack/domain--server/sqlite'
+import { sqlite_db__set } from '@rappstack/domain--server/sqlite'
+import Database from 'bun:sqlite'
 import { is_entry_file_ } from 'ctx-core/fs'
 import { migrate } from 'drizzle-orm/bun-sqlite/migrator'
 import { dirname, join } from 'node:path'
 import { app_ctx } from 'relysjs/server'
 if (is_entry_file_(import.meta.url), process.argv[1]) {
 	const dir = dirname(new URL(import.meta.url).pathname)
-	sqlite_db__name__set(app_ctx, join(dir, 'app.db'))
-	await migrate(drizzle_db_(app_ctx), { migrationsFolder: dir })
+	const sqlite_db = new Database(join(dir, 'app.db'))
+	sqlite_db.exec('PRAGMA journal_mode = WAL;')
+	sqlite_db__set(app_ctx, sqlite_db)
+	migrate(drizzle_db_(app_ctx), { migrationsFolder: dir })
 }
