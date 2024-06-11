34293516915a9683dc0bcba32cdbd9faac567cc7
Brian Takita <brian.takita@gmail.com>
2024-06-11 03:57:13 -0400

    ./tailwind.config: extend: colors:
    	+ card-indigo
    	+ card-l0-tl
    	+ card-l0-tr
    	+ card-l0-br
    	+ card-l0-bl
    	+ card-r0-tl
    	+ card-r0-tr
    	+ card-r0-br
    	+ card-r0-bl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,19 @@ const config:Config = {
 			sans: ['Urbanist'],
 			serif: ['Urbanist'],
 		},
+		extend: {
+			colors: {
+				'card-indigo': '#5f5786',
+				'card-l0-tl': '#69a84f',
+				'card-l0-tr': '#dcebd4',
+				'card-l0-br': '#d1e5ca',
+				'card-l0-bl': '#0597a4',
+				'card-r0-tl': '#e2cd79',
+				'card-r0-tr': '#e2cd79',
+				'card-r0-br': '#0597a4',
+				'card-r0-bl': '#9fc076',
+			}
+		}
 	},
 	plugins: [typography],
 }
